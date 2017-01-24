2d6f093ed02acc5b155a04dbb7c34f542c5d672e
Andrew Mcbryde <andrew.devin.mcbryde@live.mercer.edu>
2017-01-24 12:40:51 -0500

    3D area can now be used, axis placed at origin point

--- main.cpp
+++ main.cpp
@@ -16,29 +16,56 @@ const float WORLD_COORD_MAX_Y = 400.0;
 
 void myGlutInit(int argc, char** argv){
 
-    glutInit(&argc,argv);
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(WINDOW_MAX_X,WINDOW_MAX_Y);
-    glutInitWindowPosition(WINDOW_POSITION_X, WINDOW_POSITION_Y);
-    glutCreateWindow("Game of Drones");
+	glutInit(&argc,argv);
+	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(WINDOW_MAX_X,WINDOW_MAX_Y);
+	glutInitWindowPosition(WINDOW_POSITION_X, WINDOW_POSITION_Y);
+	glutCreateWindow("Game of Drones");
 
+	glEnable(GL_DEPTH_TEST);
 }
 
 void init(){
 
-        glClearColor ( 1.0, 1.0, 1.0, 0.0);
-	glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor ( 0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glShadeModel(GL_FLAT);
 	glFlush();
 
         glMatrixMode(GL_PROJECTION);
-        gluOrtho2D(WORLD_COORD_MIN_X, WORLD_COORD_MAX_X,
-                   WORLD_COORD_MIN_Y, WORLD_COORD_MAX_Y);
-
-        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+	gluPerspective(45,1.0,1.5,500);
+	glMatrixMode(GL_MODELVIEW);
 
 }
 
 void display(){
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        gluLookAt(100,100,100, 0.0,0.0,0.0, 0.0,0.0,1.0);
+
+        glPointSize(1.0);
+
+	glColor3f(1.0,1.0,1.0);
+
+	glBegin(GL_LINES);
+                glVertex3f(-50,0,0);
+                glVertex3f(50,0,0);
+        glEnd();
+
+        glBegin(GL_LINES);
+                glVertex3f(0,-50,0);
+                glVertex3f(0,50,0);
+        glEnd();
+
+        glBegin(GL_LINES);
+                glVertex3f(0,0,-50);
+                glVertex3f(0,0,50);
+        glEnd();
+
+	glFlush();
 
 }
 
@@ -47,6 +74,14 @@ void mouse(int button, int state, int x, int y){
 }
 
 void keyboard(unsigned char key, int x, int y){
+
+	if(key == 'q'){
+		exit(0);
+	}
+
+}
+
+void reshape(int w, int h){
 
 }
 
@@ -58,6 +93,8 @@ int main(int argc, char** argv){
         glutMouseFunc(mouse);
         glutKeyboardFunc(keyboard);
         glutDisplayFunc(display);
+ 	glutReshapeFunc(reshape);
+	//glutIdleFunc();
         glutMainLoop();
 
 	return 0;
