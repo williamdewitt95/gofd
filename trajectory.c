2eb51d3c7187c038f723aa503d97cca761e107c4
Andrew J. Pounds <pounds_aj@mercer.edu>
2017-02-01 19:24:11 -0500

    Converted to SI and added realistic units for tank gun and shell based on 155 mm Howitzer data.  Also added function for drag coefficient as a function of speed from unclassified military document for the 155 mm Howitzer shell.

--- trajectory.c
+++ trajectory.c
@@ -12,16 +12,17 @@
 
 // Program Constants 
 
-double V, angle, G, C, h;
+double V, m, angle, G, C, h;
 double t, y, x;
 double p, q;
 
 void init(){
  
-V     = 500.0;   // Initial Velocity            (ft/sec)
+V     = 500.0;   // Initial Velocity            (m/sec)
+m     = 50.00;   // Projectile Mass             (kg)
 angle = 75.0;    // Angle of Gun above azimuth  (degrees)
-G     = 32.0;    // Acceleration due to Graviey (ft/sec^2)
-C     = 0.0025;  // Coefficient of friction due to air
+G     = 9.81;    // Acceleration due to Graviey (ft/sec^2)
+C     = 0.05;  // Drag Coefficient 
 
 h     = 0.00001;   // Stepsize for RK4 method
 
@@ -39,14 +40,53 @@ q = V*sin(angle*M_PI/180.0);
 }
 
 
+// Cpmpute the drag based on the 155 mm HE Shell
+double drag( double speed ){
+    double mach, mach2, mach3, Kd;
+    mach = speed / 340.29;   // Speed of sound in m/s
+        mach2 = mach*mach;
+    mach3 = mach*mach*mach; 
+    if ( mach <= 0.90 ) { 
+        Kd = 0.0579080038;
+    }
+    else if ( mach <= 0.96 ) {
+        Kd = 44.26302428-138.8317032*mach+
+            144.7922431*mach2-50.12112525*mach3;
+    }
+    else if ( mach <= 1.02 ) {
+        Kd = 414.3525910-1262.4596066*mach+
+            1280.9650680*mach2-432.7252631*mach3;
+    }
+    else if ( mach <= 1.22 ) {
+        Kd = -0.4212674799+1.029831902*mach-0.4621525076*mach2;
+    }
+    else if ( mach <= 1.3 ) {
+        Kd = 8.911875950-20.35167306*mach+15.77773047*mach2-
+            4.085776562*mach3;
+    }
+    else if ( mach <= 2.6 ) {
+        Kd = (0.9415+0.1327*mach);
+        Kd = (Kd*Kd-1.0)/mach2;
+    }
+    else {
+        Kd = 0.05;
+    }
+    return Kd;
+}
+
+
 // Function for motion in horizontal direction
 double f ( double p, double q, double C ){
-	return -C*p*sqrt(pow(p,2)+pow(q,2));
+    double speed;
+    speed = sqrt(p*p+q*q);
+	return -drag(speed)*p*speed/m;
     }
 
 // Function for motion in vertical direction
 double  g ( double p, double q, double C, double G){
-    return -C*q*sqrt(pow(p,2)+pow(q,2))-G;
+    double speed;
+    speed = sqrt(p*p+q*q);
+    return -drag(speed)*q*speed/m-G;
     }
 
 
