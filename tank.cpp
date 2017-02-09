03db7f3aad9b0514986ab282516ff4b8cc33be78
Grayson Wyker <gwyker@yahoo.com>
2017-02-09 12:54:48 -0500

    tank also has a roof

--- tank.cpp
+++ tank.cpp
@@ -83,6 +83,26 @@ Tank::Tank(Point center){
 		texs.push_back(Point(1,0,0));
 		texs.push_back(Point(0,0,0));
 	}
+	{
+		sides.push_back(Polygon3d());
+		auto &points = sides[sides.size()-1].getPoints();
+		auto &texs = sides[sides.size()-1].getTexturePoints();
+		loadTex("cloud.png");
+		sides[sides.size()-1].setTexture(GLOBAL.TEXTURES_LOADED["cloud.png"].textureRef);
+		sides[sides.size()-1].setColor(211,211,211);
+		sides[sides.size()-1].setTesselation(true);
+
+		points.push_back(Point(  -1,  -1,  1));
+		points.push_back(Point(  -1,  1,   1));
+		points.push_back(Point(   1,  1,   1));
+		points.push_back(Point(   1, -1,  1));
+		points.push_back(Point(  -1,  -1,  1));
+		texs.push_back(Point(0,0,0));
+		texs.push_back(Point(0,1,0));
+		texs.push_back(Point(1,1,0));
+		texs.push_back(Point(1,0,0));
+		texs.push_back(Point(0,0,0));
+	}
 }
 
 
